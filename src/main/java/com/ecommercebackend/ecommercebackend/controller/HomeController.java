ef5c8f271b0486ee50b62a6fe04d198bbaf69b70
NYN31 <noyoncse310197@gmail.com>
2021-08-10 09:37:49 +0600

    initital commit: facing proble on mapping request solved

--- src/main/java/com/ecommercebackend/ecommercebackend/controller/HomeController.java
+++ src/main/java/com/ecommercebackend/ecommercebackend/controller/HomeController.java
@@ -2,12 +2,15 @@ package com.ecommercebackend.ecommercebackend.controller;
 
 import com.ecommercebackend.ecommercebackend.entities.BuyerRegDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
 @RestController
 public class HomeController {
     private List<BuyerRegDetails> buyers = new ArrayList<>() ;
+
     @GetMapping("/")
     public String index() {
         return "Welcome to my page"  ;
@@ -15,15 +18,19 @@ public class HomeController {
 
     @GetMapping("/buyers")
     public List<BuyerRegDetails> getAllBuyers() {
-        List<BuyerRegDetails> buyers = allBuyers() ;
+        buyers = allBuyers() ;
         return buyers;
     }
 
+    @PostMapping("/buyers")
+    public List<BuyerRegDetails> addBuyer(@RequestBody BuyerRegDetails buyer) {
+        buyers.add(buyer) ;
+        return buyers ;
+    }
     private List<BuyerRegDetails> allBuyers() {
-        List<BuyerRegDetails> list = null;
-        list.add(new BuyerRegDetails(1, "noyon", "noyon@gmail.com")) ;
-        list.add(new BuyerRegDetails(2, "rakib", "rakib@gmail.com")) ;
-        list.add(new BuyerRegDetails(3, "samin", "samin@gmail.com")) ;
-        return list ;
+        buyers.add(new BuyerRegDetails(1, "noyon", "noyon@gmail.com")) ;
+        buyers.add(new BuyerRegDetails(2, "rakib", "rakib@gmail.com")) ;
+        buyers.add(new BuyerRegDetails(3, "samin", "samin@gmail.com")) ;
+        return buyers ;
     }
 }
